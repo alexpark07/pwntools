69cd4e3e915a5775b847174841d18a481ec6ac93
alexpark <saintlinu@gmail.com>
2014-08-03 17:46:32 +0900

    added arm shellcodes (open_file, read/write_stack)

---
+++ pwnlib/shellcraft/templates/arm/linux/read_stack.asm
@@ -0,0 +1,25 @@
+<% from pwnlib.shellcraft import arm, common %>
+<% from pwnlib.util.misc import align %>
+<%page args="in_fd=None, size=255, allocate_stack=True" />
+<%docstring>
+Reads to the stack.
+
+You can optioanlly shave a few bytes not allocating the stack space.
+
+Args: 
+	in_fd (imm/reg)       = STDIN_FILENO
+	size (int/str)        = 255
+	allocate_stack(bool)  = True
+</%docstring>
+
+%if type(size) == str:
+	size = int(size)
+%endif
+
+%if allocate_stack:
+	sub sp, ${align(4, size)}
+%endif
+	mov r0, ${in_fd}
+	mov r2, ${size}
+	mov r1, sp
+	svc SYS_read
